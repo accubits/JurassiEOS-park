6e5b189f2d38d570173be583bea4d90e5be38659
Goutham.Krishna <goutham.krishna@dubailand.gov.ae>
2018-09-29 14:50:18 +0400

    basic docker compose

--- scripts/init.sh
+++ scripts/init.sh
@@ -40,10 +40,4 @@ else
     script="./scripts/newBlockchain.sh"
 fi
 
-echo "=== run docker container from the eosio/eos-dev image ==="
-docker run --rm --name eosio_notechain_container -d \
--p 8888:8888 -p 9876:9876 \
---mount type=bind,src="$(pwd)"/contracts,dst=/opt/eosio/bin/contracts \
---mount type=bind,src="$(pwd)"/scripts,dst=/opt/eosio/bin/scripts \
---mount type=bind,src="$(pwd)"/data,dst=/mnt/dev/data \
--w "/opt/eosio/bin/" eosio/eos-dev:v1.2.5 /bin/bash -c "$script"
+
